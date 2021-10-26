17ac6dade55f958f97c4648cdc9821f45c4682f4
Howard Huang <h.huang65@gmail.com>
2021-10-26 15:07:11 -0700

    Fix installation script

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@
 
 mkdir -p "${HOME}/.config/bat"
 ln -sf "${PWD}/config" "${HOME}/.config/bat/config"
-ln -sf "${PWD}/themes" "${HOME}/.config/bat/themes"
+ln -sf "${PWD}/themes" "${HOME}/.config/bat/"
+
+if hash bat 2> /dev/null; then
+  bat cache --build
+fi
